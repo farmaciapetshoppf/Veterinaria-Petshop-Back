4f6fe84cb66482b76c08ea427fb44543adac152a
tomasecovas <tomascovas496@gmail.com>
2025-12-17 17:37:00 -0300

    feature: guards in appointments

--- src/appointments/appointments.module.ts
+++ src/appointments/appointments.module.ts
@@ -15,6 +15,7 @@ import { GeneralMedicationsModule } from 'src/general-medications/general-medica
 import { GeneralMedication } from 'src/general-medications/entities/general-medication.entity';
 import { MedicationUsageHistory } from 'src/general-medications/entities/medication-usage-history.entity';
 import { AdminNotification } from 'src/general-medications/entities/admin-notification.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
@@ -32,6 +33,7 @@ import { AdminNotification } from 'src/general-medications/entities/admin-notifi
     PetsModule,
     MailerModule,
     GeneralMedicationsModule,
+    AuthModule,
   ],
   controllers: [AppointmentsController],
   providers: [AppointmentsService, AppointmentsAnalyticsSeeder],
