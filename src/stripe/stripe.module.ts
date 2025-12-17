9764f174ee7785fe998c516c42c6d48f1ccd0602
tomasecovas <tomascovas496@gmail.com>
2025-12-17 20:21:46 -0300

    fix: stripe module imports UsersModule

--- src/stripe/stripe.module.ts
+++ src/stripe/stripe.module.ts
@@ -4,12 +4,14 @@ import { StripeController } from './stripe.controller';
 import { SaleOrdersModule } from 'src/sale-orders/sale-orders.module';
 import { MailerModule } from 'src/mailer/mailer.module';
 import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
     forwardRef(() => SaleOrdersModule),
     MailerModule,
     forwardRef(() => AuthModule),
+    UsersModule,
   ],
   providers: [StripeService],
   controllers: [StripeController],
